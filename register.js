4ecc469a10c21d758ab3d391bdac4b66638c2272
Ankita Dubey <155213532+ankitadubey2004@users.noreply.github.com>
2024-07-28 08:15:50 +0530

    push

--- register.js
+++ register.js
@@ -1,14 +1,9 @@
 function validateForm() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirm-password").value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
 
     if (password !== confirmPassword) {
-        alert("Passwords do not match.");
-        return false;
-    }
-    
-    if (!document.getElementById("terms").checked) {
-        alert("You must agree to the terms and conditions.");
+        alert('Passwords do not match.');
         return false;
     }
 
